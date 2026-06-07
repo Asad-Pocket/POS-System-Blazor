f059312da081f114380a76b4066cb75385d74faa
Asad-Pocket <99897658+Asad-Pocket@users.noreply.github.com>
2026-06-07 09:53:26 +0600

    DbContext Added

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorCrudApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrudApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Sales> Sales => Set<Sales>();
+    }
+}
